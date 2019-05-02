78a09a69781d7a30322126bce23f43af8de7ccf6
Brian Quiroz <r229q057@ku.edu>
2019-05-02 18:41:07 -0500

    fixed accuracy

--- accuracy.py
+++ accuracy.py
@@ -1,8 +1,19 @@
-import model
+# from model import initializeFilter
 import cnn
 import operator
 import softmax as sm
 import numpy as np
+
+
+
+def initializeFilter(size, scale = 1.0):
+    stddev = scale/np.sqrt(np.prod(size))
+    return np.random.normal(loc = 0, scale = stddev, size = size)
+
+def initializeWeight(size):
+    return np.random.standard_normal(size=size) * 0.01
+
+
 
 def feed_forward(img, label, params, conv_s, pool_f, pool_s):
 
@@ -43,29 +54,104 @@ def feed_forward(img, label, params, conv_s, pool_f, pool_s):
 
     probs = sm.softmax(out)
 
+    return probs
 
-def accuracy(num_classes = 10, lr = 0.01, beta1 = 0.95, beta2 = 0.99, img_dim = 28, img_depth = 1, f = 2, num_filt1 = 8, num_filt2 = 8, num_filt3 = 8, num_filt4 = 8, num_filt5 = 8, batch_size = 32, num_epochs = 1, save_path = 'test.pkl'):
+
+def intermediate(batch, num_classes, lr, dim, n_c, beta1, beta2, params, cost):
+    X = batch[:,0:-1] # get batch inputs
+    X = X.reshape(len(batch), n_c, dim, dim)
+    Y = batch[:,-1] # get batch labels
+    batch_size = len(batch)
+
+    accurate = []
+    for i in range(batch_size):
+        x = X[i]
+        # print("x", len(x))
+#        print("\n\nHERE\n\n",num_classes, int(Y[i]))
+#        print(Y.shape)
+        y = np.eye(num_classes)[int(Y[i])].reshape(num_classes, 1) # convert label to one-hot
+        # print("y", len(y))
+        # Collect Gradients for training example
+        probs = feed_forward(x, y, params, 1, 2, 2)
+
+        # maxProb = max(probs)
+        index, value = max(enumerate(probs), key=operator.itemgetter(1))
+        ind, val = max(enumerate(y), key=operator.itemgetter(1))
+
+        print("THIS", index, y)
+        if index == ind:
+            accurate.append(1)
+        else:
+            accurate.append(0)
+
+    return accurate
+
+def accuracy(num_classes = 10, lr = 0.01, beta1 = 0.95, beta2 = 0.99, img_dimen = 64, img_depth = 3, f = 2, num_filt1 = 8, num_filt2 = 8, num_filt3 = 8, num_filt4 = 8, num_filt5 = 8, batch_size = 32, num_epochs = 1, save_path = 'test.pkl'):
     # training data
+    # Change string IDs to unique consec numbers
+    with open('wnids.txt') as file: #get relevant 200 ids
+        ids = [line.rstrip('\n') for line in file]
+    ids = ids[0:10]
+    print(ids)
+
+    lines = None
+    with open('words.txt') as file:
+        lines = [line.rstrip('\n') for line in file]
+    #pp.pprint(lines)
+    label_dict = {}
+    id_num = 0
+    for line in lines: #map n* id to numbers 0 to 9
+        id = line[0:line.index('\t')]
+        label = line[line.index('\t')+1:]
+        if id not in label_dict and id in ids:
+            label_dict[id] = id_num
+
+    #pp.pprint(label_dict)
+    #print(label_dict)
+
     m =500
-    X = model.extract_data('train-images-idx3-ubyte.gz', m, img_dim)
-    y_dash = model.extract_labels('train-labels-idx1-ubyte.gz', m).reshape(m,1)
-    print(y_dash)
+    data = np.load('extra-tiny-imagenet.npz')
+    X = data['train'].astype(np.float32)
+    #pp.pprint(X)
+    y = []
+    temp_y = data['labels']
+    print("temp shape",temp_y.shape)
+    for label in temp_y:
+        if label in ids:
+            y.append(label)
+
+    for i in range(len(y)):
+        y[i] = label_dict[y[i]]
+    y = np.array(y)
+    print(y.shape)
+    y = y.astype(np.float32)
+ #   for i in range(len(y)):
+ #       print(y[i])
+    num_images = X.shape[0]
+    img_len = X.shape[1]
+    img_dim = X.shape[-1]
+    print("before:", X.shape, y.shape)
+    X = X.reshape(num_images,img_len*img_len*img_dim)
+    y = y.reshape(num_images,1) # (100000,) -> (100000,1)
+    #train_data = np.hstack((X,y))
+
+    # y_dash = extract_labels('train-labels-idx1-ubyte.gz', m).reshape(m,1)
     X-= int(np.mean(X))
     X/= int(np.std(X))
-    print(X.shape, y_dash.shape)
-    train_data = np.hstack((X,y_dash))
-    print(train_data.shape)
+    print("after", X.shape, y.shape)
+    train_data = np.hstack((X,y))
+    # print(train_data.shape)
 
     np.random.shuffle(train_data)
 
-    f1, f2, f3, f4, f5, w6, w7 = (num_filt1 ,img_depth,f,f), (num_filt2 ,num_filt1,f,f), (num_filt3, num_filt2, f, f), (num_filt4, num_filt3, f, f), (num_filt5, num_filt4, f, f), (128,8), (10, 128)
-    f1 = model.initializeFilter(f1)
-    f2 = model.initializeFilter(f2)
-    f3 = model.initializeFilter(f3)
-    f4 = model.initializeFilter(f4)
-    f5 = model.initializeFilter(f5)
-    w6 = model.initializeWeight(w6)
-    w7 = model.initializeWeight(w7)
+    f1, f2, f3, f4, f5, w6, w7 = (num_filt1 ,img_depth,f,f), (num_filt2 ,num_filt1,f,f), (num_filt3, num_filt2, f, f), (num_filt4, num_filt3, f, f), (num_filt5, num_filt4, f, f), (128,288), (10, 128)
+    f1 = initializeFilter(f1)
+    f2 = initializeFilter(f2)
+    f3 = initializeFilter(f3)
+    f4 = initializeFilter(f4)
+    f5 = initializeFilter(f5)
+    w6 = initializeWeight(w6)
+    w7 = initializeWeight(w7)
 
     b1 = np.zeros((f1.shape[0],1))
     b2 = np.zeros((f2.shape[0],1))
@@ -81,12 +167,13 @@ def accuracy(num_classes = 10, lr = 0.01, beta1 = 0.95, beta2 = 0.99, img_dim = 
 
     print("LR:"+str(lr)+", Batch Size:"+str(batch_size))
 
-    probs = feed_forward(train_data, num_classes, lr, img_dim, img_depth, beta1, beta2, params, cost)
-    print(probs)
-
-    maxProb = max(probs)
-    index, value = max(enumerate(my_list), key=operator.itemgetter(1))
-    print(index)
+    accurate = intermediate(train_data, num_classes, lr, img_dimen, img_depth, beta1, beta2, params, cost)
+    print(accurate)
+    # print(probs)
+    #
+    # maxProb = max(probs)
+    # index, value = max(enumerate(my_list), key=operator.itemgetter(1))
+    # print(index)
 
 
 
