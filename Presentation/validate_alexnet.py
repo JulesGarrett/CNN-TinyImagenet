5c96b4b2a577944ccad22961d7c2c0ef1335492b
dineshmukherjee <47728741+dineshmukherjee@users.noreply.github.com>
2019-05-02 14:16:03 -0500

    Add files via upload

--- Presentation/validate_alexnet.py
+++ Presentation/validate_alexnet.py
@@ -1,44 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from win32api import GetSystemMetrics
+import classify as cls
 
 import cv2
 #from alexnet import alexnet
 
 
 images = []
-dogimg = cv2.imread('image_dog.png')##what's type of image in imagent
-# birdimg = cv2.imread('image_bird.png')
-# appleimg = cv2.imread('image_apple.png')
-images.append(dogimg)
-# images.append(birdimg)
-# images.append(appleimg)
+dogimg = cv2.imread('C:\\Users\\Admin\\Documents\\GitHub\\CNN-TinyImagenet\\Presentation\\image_dog.png')##what's type of image in imagent
+inputimg = cv2.imread('C:\\Users\\Admin\\Documents\\GitHub\\CNN-TinyImagenet\\Presentation\\image_dog.png',0)##what's type of image in imagent
 
-predictions = [];#use to store probabilities for each image
+screen_res =  GetSystemMetrics(0),  GetSystemMetrics(1)
+scale_width = screen_res[0] / dogimg.shape[1]
+scale_height = screen_res[1] / dogimg.shape[0]
+scale = min(scale_width, scale_height)
+window_width = int(dogimg.shape[1] * scale)
+window_height = int(dogimg.shape[0] * scale)
 
-## apply to pretrained model
-#for i in enumerate(images):
-#    model = alexnet(images)
-#    predictions.append(model)
-predictions.append( [{'dog':0.9},{'bird':0.12},{'apple':0.05}])
-# predictions.append( [{'bird':0.87},{'apple':0.33},{'bird':0.1}])
-# predictions.append( [{'apple':0.88},{'bird':0.19},{'dog':0.15}])
+cv2.namedWindow('dst_rt', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('dst_rt', window_width, window_height)
 
+cv2.imshow('dst_rt', dogimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-## plot name of class and confidence
-# dogimg = cv2.resize(dogimg, (1800,2000))
-plt.subplot(221)
-plt.imshow(cv2.cvtColor(dogimg, cv2.COLOR_BGR2RGB))
-plt.title('Dog')
-# plt.text(0, 2000, str(predictions[0]), size=10, rotation=0.,ha="left",bbox=dict(boxstyle="round",ec=(1., 0.5, 0.5),fc=(1., 0.8, 0.8),))
-plt.axis('off')
+inputimg=cv2.resize(inputimg,(28,28),interpolation = cv2.INTER_AREA)
+inputimg = np.expand_dims(inputimg, axis=0) 
+
+prob = cls.classify(inputimg,[],[], 1, 2, 2)
 #
-# plt.show()
-
-
-#Bar Graph
-x = [u'Dog', u'Cat', u'Apple', u'Bird', u'Chair', u'Lion', u'Shark', u'Table']
-y = [0.04, 0.16, 0.14, 0.05, 0.15, 0.01, 0.03, 0.06]
+## birdimg = cv2.imread('image_bird.png')
+## appleimg = cv2.imread('image_apple.png')
+#images.append(dogimg)
+## images.append(birdimg)
+## images.append(appleimg)
+#
+#predictions = [];#use to store probabilities for each image
+#
+### apply to pretrained model
+##for i in enumerate(images):
+##    model = alexnet(images)
+##    predictions.append(model)
+#predictions.append( [{'dog':0.9},{'bird':0.12},{'apple':0.05}])
+## predictions.append( [{'bird':0.87},{'apple':0.33},{'bird':0.1}])
+## predictions.append( [{'apple':0.88},{'bird':0.19},{'dog':0.15}])
+#
+#
+### plot name of class and confidence
+## dogimg = cv2.resize(dogimg, (1800,2000))
+#plt.subplot(221)
+#plt.imshow(cv2.cvtColor(dogimg, cv2.COLOR_BGR2RGB))
+#plt.title('Dog')
+## plt.text(0, 2000, str(predictions[0]), size=10, rotation=0.,ha="left",bbox=dict(boxstyle="round",ec=(1., 0.5, 0.5),fc=(1., 0.8, 0.8),))
+#plt.axis('off')
+##
+## plt.show()
+#
+#
+##Bar Graph
+x = [u'Dog', u'Cat', u'Apple', u'Bird', u'Chair', u'Lion', u'Shark', u'Table',u'Map']
+y = prob.tolist()
 y.sort(reverse=True)
 
 fig, ax = plt.subplots()
